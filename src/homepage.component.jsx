143a90bade20f50ef67b8923ff9d37be4da70e31
Anel <61626682+AnelDeveloper@users.noreply.github.com>
2021-08-25 21:25:24 +0200

    created project with create app and added files for homepage

---
+++ src/homepage.component.jsx
@@ -0,0 +1,53 @@
+import React from 'react';
+
+import './homepage.style.scss';
+
+
+const HomePage = () => (
+
+    <div className='homepage'>
+    <div className='directory-menu'>
+    <div className='menu-item'>
+    <div className='content'>
+    <h1 className='tittle'>HATS</h1>
+    <span className='subtittle'>SHOP NOW</span>
+    
+    </div>
+     </div>
+
+     <div className='menu-item'>
+     <div className='content'>
+     <h1 className='tittle'>JACKETZ</h1>
+     <span className='subtittle'>SHOP NOW</span>
+     
+     </div>
+      </div>
+
+      <div className='menu-item'>
+      <div className='content'>
+      <h1 className='tittle'>SNEAK</h1>
+      <span className='subtittle'>SHOP NOW</span>
+      
+      </div>
+       </div>
+
+       <div className='menu-item'>
+       <div className='content'>
+       <h1 className='tittle'>WOMEN</h1>
+       <span className='subtittle'>SHOP NOW</span>
+       
+       </div>
+        </div>
+
+        <div className='menu-item'>
+        <div className='content'>
+        <h1 className='tittle'>MENS</h1>
+        <span className='subtittle'>SHOP NOW</span>
+        
+        </div>
+         </div>
+    </div>
+    </div>
+);
+
+export default HomePage;
